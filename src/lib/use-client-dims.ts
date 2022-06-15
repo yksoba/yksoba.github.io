32fa1d96238b291517a1352d7d40590ec22db626
koreanwglasses <koreanwglasses@gmail.com>
2022-06-15 18:18:05 -0500

    wip

---
+++ src/lib/use-client-dims.ts
@@ -0,0 +1,20 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
+export const useClientDims = <T extends HTMLElement=HTMLElement>() => {
+  const ref = useRef<T>(null);
+  const [clientDims, setClientDims] = useState({
+    clientWidth: 0,
+    clientHeight: 0,
+  });
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      setClientDims({
+        clientHeight: ref.current.clientHeight,
+        clientWidth: ref.current.clientWidth,
+      });
+    }
+  }, [ref.current?.clientHeight, ref.current?.clientWidth]);
+
+  return [ref, clientDims] as const;
+};
--- src/pages/comms.tsx
+++ src/pages/comms.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { PropsWithChildren, ReactNode } from "react";
+import { ReactNode } from "react";
 import { Layout } from "../components/layout";
-import { Flex, FlexCol } from "../components/flex";
+import { FlexCol, Flex } from "../components/flex";
 import icon1 from "../images/comms/icon-1.png";
-import halfbody from "../images/comms/half-body.png";
+import icon2 from "../images/comms/icon-2.png";
+import halfbody1 from "../images/comms/half-body-1.png";
+import halfbody2 from "../images/comms/half-body-2.png";
 import full1 from "../images/comms/full-1.png";
+import full2 from "../images/comms/full-2.png";
 import { Typography } from "@mui/material";
 import { Link } from "gatsby";
+import { useClientDims } from "../lib/use-client-dims";
 
 const Commissions = () => {
   return (
@@ -47,19 +51,55 @@ const Commissions = () => {
           imgsrc={icon1}
           bgpos="65%"
           cover={<Cover name="Icon" price="10" />}
+          content={<Content imgsrcs={[icon1, icon2]} />}
         />
         <Panel
-          imgsrc={halfbody}
+          imgsrc={halfbody1}
           bgpos="50%"
           cover={<Cover name="Half-Body" price="20" />}
+          content={<Content imgsrcs={[halfbody1, halfbody2]} />}
         />
         <Panel
           imgsrc={full1}
           bgpos="70%"
           cover={<Cover name="Full-Body" price="30" />}
+          content={<Content imgsrcs={[full1, full2]} />}
         />
       </Flex>
     </Layout>
+  );
+};
+
+const Content = ({ imgsrcs }: { imgsrcs: string[] }) => {
+  return (
+    <FlexCol justifyContent="center" width="100%" px={6}>
+      <MiniGallery imgsrcs={imgsrcs} />
+    </FlexCol>
+  );
+};
+
+const MiniGallery = ({ imgsrcs }: { imgsrcs: string[] }) => {
+  const [ref, dims] = useClientDims();
+  const size = dims.clientWidth / 2 - 8;
+
+  return (
+    <Flex width="100%" flexWrap="wrap" gap={1} ref={ref}>
+      {imgsrcs.map((imgsrc, i) => (
+        <Flex
+          key={i}
+          width={size}
+          height={size}
+          border={6}
+          borderRadius={1}
+          borderColor="white"
+          sx={{
+            background: `url("${imgsrc}")`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+      ))}
+    </Flex>
   );
 };
 
@@ -74,15 +114,16 @@ const Cover = ({ name, price }: { name: string; price: string }) => {
 };
 
 const Panel = ({
-  children,
+  content,
   cover,
   imgsrc,
   bgpos = "50%",
-}: PropsWithChildren<{
+}: {
+  content?: ReactNode;
   cover?: ReactNode;
   imgsrc: string;
   bgpos?: string;
-}>) => {
+}) => {
   return (
     <FlexCol
       flexGrow={1}
@@ -109,10 +150,22 @@ const Panel = ({
           transform: "scale(110%)",
           filter: "brightness(0.5)",
         },
+
+        "& #content": {
+          opacity: "0",
+          transition: "opacity 0.3s",
+        },
+
+        "&:hover #content": {
+          opacity: "1",
+        },
       }}
     >
-      <Flex position="relative" width="100%" height="100%">
+      <Flex position="absolute" width="100%" height="100%">
         {cover}
+      </Flex>
+      <Flex position="absolute" width="100%" height="100%" id="content">
+        {content}
       </Flex>
     </FlexCol>
   );

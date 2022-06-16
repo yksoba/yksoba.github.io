c7251a82e2ecacbf10f0d3c11a0302b867423a3b
koreanwglasses <koreanwglasses@gmail.com>
2022-06-16 18:28:57 -0500

    Updates

---
+++ component---src-pages-index-tsx-d735e00f0b90df7e8624.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[691],{3473:function(e,t,n){n.r(t);var c=n(921),l=n(7291),a=n(4687),r=n(4569);t.default=function(){return c.createElement(a.A,null,c.createElement("title",null,"yksoba"),c.createElement(r.Ht,{justifyContent:"center"},c.createElement(l.Z,{variant:"body1",ml:1},"Nothing here yet... check back soon!")))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-d735e00f0b90df7e8624.js.map

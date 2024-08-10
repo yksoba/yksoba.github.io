920e03b1b182a727eaf339f2ec329d1b6126cba6
yksoba <the.yk.soba@gmail.com>
2024-08-10 07:51:43 -0500

    changed shop link to https

--- src/pages/conventions/portfolio.tsx
+++ src/pages/conventions/portfolio.tsx
@@ -84,7 +84,7 @@ const Page = (props: PageProps) => {
           You can find photos of my previous table setups below, which will have
           the most comprehensive representation of the kind of merch I plan to
           sell. You can also find some examples at my online store:{" "}
-          <AutoLink href="https://shop.yksoba.art" />.
+          <AutoLink href="http://shop.yksoba.art" />.
         </p>
 
         <h2>At-Con Commissions</h2>
